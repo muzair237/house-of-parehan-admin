62b88a9ff4573b370ed22adbab7fb3dc3cac9a09
Muhammad Uzair <uzair.ejaz2001@gmail.com>
2025-10-22 00:03:33 +0500

    feat: fixed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { AppProviders } from './providers';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html suppressHydrationWarning>
-      <body className="font-ubuntu antialiased">
+      <body className="font-outfit antialiased">
         <AppProviders>{children}</AppProviders>
       </body>
     </html>
